5616ba16c529b011240c8546ff9de2e060088462
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-06-21 01:04:02 +0500

    Fix code

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -45,40 +45,71 @@ const Sidebar = () => {
           <span>+</span>
           <button>Reklama qo'shish</button>
         </AddAds>
-        <div>
-          <Paper sx={{ width: 230 }}>
-            <MenuList>
-              <MenuItem>
-                <ListItemIcon>
-                  <Icons.Home />
-                </ListItemIcon>
-                <Typography variant="inherit">Asosiy</Typography>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <Icons.Help />
-                </ListItemIcon>
-                <Typography variant="inherit">Yordam</Typography>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <Icons.Exit />
-                </ListItemIcon>
-                <Typography variant="inherit" noWrap>
-                  Akkountdan chiqish
-                </Typography>
-              </MenuItem>
-            </MenuList>
-          </Paper>
-        </div>
+        <Paper
+          sx={{ width: 230 }}
+          style={{
+            width: "100%",
+            backgroundColor: "transparent",
+            borderRadius: "0",
+            color: "#fff",
+            marginTop: "20px",
+            boxShadow: "none",
+          }}
+        >
+          <MenuList>
+            <MenuItem
+              style={{
+                marginBottom: "10px",
+                padding: "16px",
+              }}
+            >
+              <ListItemIcon>
+                <Icons.Home />
+              </ListItemIcon>
+              <Typography variant="inherit">Asosiy</Typography>
+            </MenuItem>
+            <MenuItem
+              style={{
+                marginBottom: "10px",
+                padding: "16px",
+              }}
+            >
+              <ListItemIcon>
+                <Icons.Help />
+              </ListItemIcon>
+              <Typography variant="inherit">Yordam</Typography>
+            </MenuItem>
+            <MenuItem
+              style={{
+                marginBottom: "10px",
+                padding: "16px",
+              }}
+            >
+              <ListItemIcon>
+                <Icons.Exit />
+              </ListItemIcon>
+              <Typography variant="inherit" noWrap>
+                Akkountdan chiqish
+              </Typography>
+            </MenuItem>
+          </MenuList>
+        </Paper>
       </div>
       <div id="language">
-        <FormControl sx={{ minWidth: 130 }}>
+        <FormControl
+          sx={{ minWidth: 130 }}
+          style={{
+            width: "20%",
+          }}
+        >
           <Select
             value={age}
             onChange={handleChange}
             displayEmpty
             inputProps={{ "aria-label": "Without label" }}
+            style={{
+              color: "#fff",
+            }}
           >
             <MenuItem value="">
               <p>Uzbek</p>
